6e533eb5894f50be1c496e0dc4adc78049cd616f
Mirlan Jelamanov <mirlan.jelamanov@gmail.com>
2022-04-15 13:37:31 +0600

    Redirects after order placed to ui

--- view/frontend/web/js/view/payment/method-renderer/factoring004.js
+++ view/frontend/web/js/view/payment/method-renderer/factoring004.js
@@ -3,15 +3,30 @@ define(
         'Magento_Checkout/js/view/payment/default',
         'jquery',
         'ko',
+        'Magento_Checkout/js/model/full-screen-loader',
     ],
-    function (Component, $, ko) {
+    function (Component, $, ko, fullScreenLoader) {
         'use strict';
         return Component.extend({
             defaults: {
                 template: 'BnplPartners_Factoring004Magento/payment/factoring004',
+                redirectAfterPlaceOrder: false,
+                _redirectLink: '',
             },
 
             termsChecked: ko.observable(false),
+
+            afterPlaceOrder () {
+                if (this._redirectLink) {
+                    fullScreenLoader.startLoader();
+                    window.location.replace(this._redirectLink);
+                }
+            },
+
+            getPlaceOrderDeferredObject () {
+                return this._super()
+                    .done((data, type, jqXHR) => this._redirectLink = jqXHR.getResponseHeader('X-Location'));
+            },
 
             agreementUrl () {
                 return window.checkoutConfig.payment.bnplpartners_factoring004magento.agreementUrl;

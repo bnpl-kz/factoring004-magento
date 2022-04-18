27db94e78ece9f5c20066d1c2ed32c96016d2fee
bnpl-partners <98588156+bnpl-partners@users.noreply.github.com>
2022-04-18 21:09:43 +0600

    Initial commit

---
+++ registration.php
@@ -0,0 +1,7 @@
+<?php
+
+declare(strict_types=1);
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'BnplPartners_Factoring004Magento', __DIR__);

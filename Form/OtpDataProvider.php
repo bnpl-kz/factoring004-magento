89b78971e97c9559988e209a4995a7d577009592
Mirlan Jelamanov <mirlan.jelamanov@gmail.com>
2022-04-21 18:17:43 +0600

    Adds do field to otp form

--- Form/OtpDataProvider.php
+++ Form/OtpDataProvider.php
@@ -5,10 +5,29 @@ declare(strict_types=1);
 namespace BnplPartners\Factoring004Magento\Form;
 
 use Magento\Framework\Api\Filter;
+use Magento\Framework\App\RequestInterface;
 use Magento\Ui\DataProvider\AbstractDataProvider;
 
 class OtpDataProvider extends AbstractDataProvider
 {
+    /**
+     * @var \Magento\Framework\App\RequestInterface
+     */
+    private $request;
+
+    public function __construct(
+        $name,
+        $primaryFieldName,
+        $requestFieldName,
+        RequestInterface $request,
+        array $meta = [],
+        array $data = []
+    ) {
+        parent::__construct($name, $primaryFieldName, $requestFieldName, $meta, $data);
+
+        $this->request = $request;
+    }
+
     public function addFilter(Filter $filter): void
     {
 
@@ -16,6 +35,12 @@ class OtpDataProvider extends AbstractDataProvider
 
     public function getData(): array
     {
-        return [];
+        $action = $this->request->getParam($this->getRequestFieldName(), 'shipment');
+
+        return [
+            $action => [
+                'fields' => [$this->getRequestFieldName() => $action],
+            ],
+        ];
     }
 }

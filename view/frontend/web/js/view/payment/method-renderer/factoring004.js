3f71c791b0f284772e4466b845e3ec25e1ce8c38
Mirlan Jelamanov <mirlan.jelamanov@gmail.com>
2022-08-15 18:44:58 +0600

    Renders payment schedule

--- view/frontend/web/js/view/payment/method-renderer/factoring004.js
+++ view/frontend/web/js/view/payment/method-renderer/factoring004.js
@@ -4,17 +4,25 @@ define(
         'jquery',
         'ko',
         'Magento_Checkout/js/model/full-screen-loader',
+        'Magento_Checkout/js/model/totals',
+        'BnplPartners_Factoring004Magento/js/view/payment/schedule/factoring004',
     ],
-    function (Component, $, ko, fullScreenLoader) {
+    function (Component, $, ko, fullScreenLoader, totals, Factoring004Payment) {
         'use strict';
         return Component.extend({
             defaults: {
                 template: 'BnplPartners_Factoring004Magento/payment/factoring004',
                 redirectAfterPlaceOrder: false,
                 _redirectLink: '',
+                schedule: '',
             },
 
             termsChecked: ko.observable(false),
+
+            initialize () {
+                this._super();
+                this.schedule = this._renderSchedule();
+            },
 
             afterPlaceOrder () {
                 if (this._redirectLink) {
@@ -38,6 +46,18 @@ define(
 
             description () {
                 return window.checkoutConfig.payment.bnplpartners_factoring004magento.description;
+            },
+
+            _renderSchedule () {
+                const elem = document.createElement('div');
+                const schedule = new Factoring004Payment.PaymentSchedule({
+                    elemId: 'payment-factoring004-schedule',
+                    totalAmount: totals.totals().grand_total,
+                });
+
+                schedule.renderTo(elem);
+
+                return elem.innerHTML;
             }
         });
     }

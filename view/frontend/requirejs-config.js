321b75408ee758ed1549799fb6ecd4971b1434cc
Mirlan Jelamanov <mirlan.jelamanov@gmail.com>
2022-11-24 17:57:24 +0600

    Adds widget script links

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -2,6 +2,8 @@ var config = {
     map: {
         '*': {
             'Magento_Checkout/js/model/error-processor': 'BnplPartners_Factoring004Magento/js/model/error-processor',
+            'bnpl-kz-modal-prod': 'https://bnpl.kz/widget/index_bundle.js',
+            'bnpl-kz-modal-dev': 'https://dev.bnpl.kz/widget/index_bundle.js',
         },
     },
 };

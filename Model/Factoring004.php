e5e070489704c7c981aa163804dcc9fbbe1d47a1
Mirlan Jelamanov <mirlan.jelamanov@gmail.com>
2023-03-02 15:37:07 +0600

    Restores payment module hidding

--- Model/Factoring004.php
+++ Model/Factoring004.php
@@ -3,6 +3,7 @@
 namespace BnplPartners\Factoring004Magento\Model;
 
 use Magento\Payment\Model\Method\AbstractMethod;
+use Magento\Quote\Api\Data\CartInterface;
 
 class Factoring004 extends AbstractMethod
 {
@@ -23,15 +24,37 @@ class Factoring004 extends AbstractMethod
      */
     protected $supportedCurrencyCodes;
 
+    /**
+     * @var int
+     */
+    protected $minOrderTotal;
+
+    /**
+     * @var int
+     */
+    protected $maxOrderTotal;
+
     protected function initializeData($data = [])
     {
         parent::initializeData($data);
 
         $this->supportedCurrencyCodes = (array) $this->getConfigData('currency');
+        $this->minOrderTotal = (int) $this->getConfigData('min_order_total');
+        $this->maxOrderTotal = (int) $this->getConfigData('max_order_total');
     }
 
     public function canUseForCurrency($currencyCode): bool
     {
         return in_array($currencyCode, $this->supportedCurrencyCodes, true);
+    }
+
+    public function isAvailable(CartInterface $quote = null): bool
+    {
+        if ($quote) {
+            return $quote->getBaseGrandTotal() >= $this->minOrderTotal
+                && $quote->getBaseGrandTotal() <= $this->maxOrderTotal;
+        }
+
+        return parent::isAvailable($quote);
     }
 }
